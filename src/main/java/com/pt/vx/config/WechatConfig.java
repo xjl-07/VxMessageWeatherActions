201976e9ad82bb40c48ec02c7ab5cf317bb628ae
xjl-07 <51904042+xjl-07@users.noreply.github.com>
2024-06-17 17:25:23 +0800

    Update WechatConfig.java

--- src/main/java/com/pt/vx/config/WechatConfig.java
+++ src/main/java/com/pt/vx/config/WechatConfig.java
@@ -39,7 +39,7 @@ public class WechatConfig {
     static {
         userList.add(getUser(
                 "okxJl6Y6vTf6gqQePKPzxYhyYky4", //扫码关注你的测试号以后，测试平台会出现TA的微信号
-                "KB4PC0xLemr7O2VBlFDJd0-GbYPt10_1QNw5YZ6ZvZo", //要给这个人发送的模板ID
+                "amznwt3UZGnT_sXL8B54vwRl3952wgGXvfQeZ3PYRKc", //要给这个人发送的模板ID
                 "宝贝", //咋称呼这个人
                 "湖南省长沙市", //这个人的详细地址
                 "长沙", //这个人在的城市
